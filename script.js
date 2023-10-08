7ccbee0b8afaf74d6268c4641ba5eb7e2af61562
iiren3 <147114749+iiren3@users.noreply.github.com>
2023-10-08 13:21:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById("login").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  // Get ur variables and stuff
+  var username = document.getElementById("Uname").value;
+  var password = document.getElementById("Pass").value;
+
+  // If username and password are... then you...
+  if (username === "djdsryjhdjnhfg" && password === "xsjfdhsuhgdjish") {
+    window.location.href = "just-a-stupid-game.cupcakeraptors1.repl.co/"; // DONT FORGET TO ADD https://
+  }
+  // You can also add this multiple times.
+  if (username === "lasgndjshndf" && password === "djsghuh") {
+    window.location.href = "https://www.google.com/";
+  }
+    if (username === "husdhgudhu" && password === "dfsjigdjis") {
+    window.location.href = "https://www.google.com/"
+  }
+      if (username === "adsfgdsdsfds" && password === "afddasfsdg") {
+    window.location.href = "https://www.google.com/"
+  }
+if (username === "......ada.." && password === "..adsa..") {
+    window.location.href = "https://www.google.com/"
+  } 
+        // If the password/username isnt correct then display a message
+  else {
+    document.getElementById("message").textContent = "Oh my, We can not accept your request currently. Please try again later. ";
+  }
+});
+document.getElementById("login").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  // Get ur variables and stuff
+  var username = document.getElementById("befr").value;
+  var password = document.getElementById("gatekeep").value;
+
+  // If username and password are... then you...
+  if (username === "Your username" && password === "Your password") {
+    window.location.href = "Your website"; // DONT FORGET TO ADD https://
+  }
+  // You can also add this multiple times.
+  if (username === "Your username 2" && password === "Your password 2") {
+    window.location.href = "Your website 2";
+  }
+  // If the password/username isnt correct then display a message
+  else {
+    document.getElementById("message").textContent = "Nope ur stuff isnt correct dummy lol";
+  }
+});
